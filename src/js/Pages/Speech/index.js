e35612fcef816b4d1ac40292defd851c6033e468
Sambego <sambellen@gmail.com>
2017-03-10 21:25:56 +0100

    Fix quotes

--- src/js/Pages/Speech/index.js
+++ src/js/Pages/Speech/index.js
@@ -6,7 +6,7 @@ import {Speech} from '../../Services';
 const SpeechPage = ({children}) => {
     const conf = 'Front Trends';
     const bubble1 = `Hi ${conf}, did you know the browser could talk? Cool huh!`;
-    const bubble2 = 'The web speech API converts text to spoken words. You can even choose my voice, since I’m a female cat, I’ll be talking with a female voice. Meow!';
+    const bubble2 = 'The web speech API converts text to spoken words. You can even choose my voice, since I\'m a female cat, I\'ll be talking with a female voice. Meow!';
 
     const speak = bubbleId => {
         switch (bubbleId) {
