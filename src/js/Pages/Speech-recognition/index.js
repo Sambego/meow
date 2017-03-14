72172e7ff4eaae7495d9a7ee09b473f60414281e
Sambego <sambellen@gmail.com>
2017-03-14 23:11:03 +0100

    Group speech recognition button with previous bubble

--- src/js/Pages/Speech-recognition/index.js
+++ src/js/Pages/Speech-recognition/index.js
@@ -30,8 +30,8 @@ export default class SpeechRecognitionPage extends Component {
     render() {
         return (
             <BubbleSlide previous="speech-code" next="speech-recognition-code" ref={linkRef(this, 'bubbleSlide')}>
-                <Bubble>So Sam, you know I can talk to you trough the browser, but did you know you can also talk to me? Thats right, I can understand the words you speak to me. Press the button below and say something.</Bubble>
                 <BubbleCollection>
+                    <Bubble>So Sam, you know I can talk to you trough the browser, but did you know you can also talk to me? Thats right, I can understand the words you speak to me. Press the button below and say something.</Bubble>
                     <SpeechRecognition onSpeechRecognitionStart={::this.onSpeechRecognitionStart} onSpeechRecognized={::this.setRecognizedSpeech}></SpeechRecognition>
                     {this.state.speechRegnitionInProcess && <Bubble me>
                         <Loader/>
