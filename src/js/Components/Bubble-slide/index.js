b8dc6d3469e3864550965c534401b863c4602c4e
Sambego <sambellen@gmail.com>
2017-03-29 12:02:57 +0200

    Add mobile nav

--- src/js/Components/Bubble-slide/index.js
+++ src/js/Components/Bubble-slide/index.js
@@ -1,7 +1,8 @@
 import {h, render, Component} from 'preact';
 import linkRef from 'linkref';
-import {Slide, Bubbles} from '../../Components';
+import {Slide, Bubbles, Icon} from '../../Components';
 import {Keyboard} from '../../Services';
+import styles from './bubble-slide.scss';
 
 export default class BubbleSlide extends Component {
     goToPreviousPage() {
@@ -58,6 +59,11 @@ export default class BubbleSlide extends Component {
                 <Bubbles ref={linkRef(this, 'bubbles')} onUpdate={::this.scroll}>
                     {this.props.children}
                 </Bubbles>
+
+                <nav className={styles['bubble-nav']}>
+                    <button className={styles['bubble-nav__prev']} onClick={::this.getPreviousAction}><Icon name="caret-left" color="#66B7F9" size="small"/></button>
+                    <button className={styles['bubble-nav__next']} onClick={::this.getNextAction}><Icon name="caret-right" color="#66B7F9" size="small" /></button>
+                </nav>
             </Slide>
         );
     }
